6e931c26813c507cfa6545ee677212867f2e0894
Yernar <ernar@agrostream.net>
2018-06-04 17:34:25 +0600

    first commit

---
+++ src/components/MainPage.vue
@@ -0,0 +1,58 @@
+<template lang="pug">
+  #main-page
+    .first-block
+      .navigation
+        .nav-logo
+          img(src="@/assets/logo-header.png")
+        .navs
+          .nav(v-for="nav in navs") {{nav.text}}
+
+
+</template>
+
+<script>
+export default {
+  name: 'MainPage',
+  data () {
+    return {
+      navs: [
+        {
+          id: 0,
+          text: 'ГЛАВНАЯ',
+          url: ''
+        },
+        {
+          id: 1,
+          text: 'AGROSTREAM',
+          url: ''
+        },
+        {
+          id: 2,
+          text: 'ФУНКЦИОНАЛ',
+          url: ''
+        },
+        {
+          id: 3,
+          text: 'ОТЗЫВЫ',
+          url: ''
+        },
+        {
+          id: 4,
+          text: 'МОДУЛИ',
+          url: ''
+        },
+        {
+          id: 5,
+          text: 'КОНТАКТЫ',
+          url: ''
+        },
+        {
+          id: 6,
+          text: 'О КОМПАНИИ',
+          url: ''
+        },
+      ]
+    }
+  }
+}
+</script>
